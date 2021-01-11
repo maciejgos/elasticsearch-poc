b305b2f755b8b9f8b6da8e82de0305d4f749dc2b
Maciej Gos <maciej@maciejgos.com>
2021-01-11 11:20:49 +0100

    Add script

---
+++ src/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker-compose up -d
+
+(echo -n '{"filename":faktura_3_2020_31-12-2020.pdf", "data": "'; base64 ./data/faktura_3_2020_31-12-2020.pdf; echo '"}') | curl -H "Content-Type: application/json" -d @-  http://localhost:9200/documents/_doc/1?pipeline=documents
+(echo -n '{"filename":zbiorczyWydrukZa_grudzien_2020 (1).pdf", "data": "'; base64 ./"data/zbiorczyWydrukZa_grudzien_2020 (1).pdf"; echo '"}') | curl -H "Content-Type: application/json" -d @-  http://localhost:9200/documents/_doc/2?pipeline=documents
+(echo -n '{"filename":zbiorczyWydrukZa_listopad_2020.pdf", "data": "'; base64 ./"data/zbiorczyWydrukZa_listopad_2020.pdf"; echo '"}') | curl -H "Content-Type: application/json" -d @-  http://localhost:9200/documents/_doc/3?pipeline=documents
